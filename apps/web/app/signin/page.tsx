7238d107006c38ec5fd854d7571e3f366b38296d
mdhasan76 <mdhasan8064@gmail.com>
2025-02-07 19:18:25 +0600

    nav, route, and page desing

--- apps/web/app/signin/page.tsx
+++ apps/web/app/signin/page.tsx
@@ -1,9 +1,42 @@
-
-import React from "react";
-
-const page = () => {
-  return <div className="h-96 w-96 bg-red-500">Sign in page</div>;
-};
-//
-
+function page() {
+  return (
+    <form className="max-w-sm mx-auto">
+      <div className="mb-5">
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light"
+          placeholder="name@flowbite.com"
+          required
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="password"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your password
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Login
+      </button>
+    </form>
+  );
+}
 export default page;
