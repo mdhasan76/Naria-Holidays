20bc5695616091a18d53b8b69ea7801c7904f488
mdhasan76 <mdhasan8064@gmail.com>
2025-02-07 16:06:18 +0600

    setup tailwind flowbite

---
+++ apps/web/app/signin/page.tsx
@@ -0,0 +1,18 @@
+import { Dropdown } from "flowbite-react";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      {" "}
+      <Dropdown label="Dropdown button">
+        <Dropdown.Item>Dashboard</Dropdown.Item>
+        <Dropdown.Item>Settings</Dropdown.Item>
+        <Dropdown.Item>Earnings</Dropdown.Item>
+        <Dropdown.Item>Sign out</Dropdown.Item>
+      </Dropdown>
+    </div>
+  );
+};
+
+export default page;
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,11 +1,22 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 // tailwind config is required for editor support
 
 import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
-
-const config: Pick<Config, "content" | "presets"> = {
-  content: ["./app/**/*.tsx"],
-  presets: [sharedConfig],
+const config: Pick<Config, "content" | "presets" | "plugins" | "theme"> = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "glow-conic":
+          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
 };
 
 export default config;
